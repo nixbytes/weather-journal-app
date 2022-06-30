f8adbc067ba063922abac193c018e5fe36ee4e1a
nixbytes <real8686@gmail.com>
2022-06-29 22:17:27 -0400

    add requires and cors

--- server.js
+++ server.js
@@ -2,20 +2,21 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('require');
 
 // Start up an instance of app
+const app = express()
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
 
 // Initialize the main project folder
 app.use(express.static("website"));
 
 // Setup Server
-const server = app.listen(port, () => {
-	console.log(`running on localhost: ${port}`);
-});
