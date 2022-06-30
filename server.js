26afc0460affd671ec6b7ca19f383c19df2b2273
nixbytes <real8686@gmail.com>
2022-06-29 22:36:42 -0400

    add server function

--- server.js
+++ server.js
@@ -2,21 +2,28 @@
 projectData = {};
 
 // Require Express to run server and routes
-const express = require('require');
+const express = require("express");
 
 // Start up an instance of app
-const app = express()
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
 
 // Initialize the main project folder
 app.use(express.static("website"));
 
+const port = 3000;
+
 // Setup Server
+
+const server = app.listen(port, listening);
+function listening() {
+	console.log("running server on port: ${port}");
+}
