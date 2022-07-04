cdf7054ad9cfbfeb7de0c14fc1c4f66423c4fc10
nixbytes <real8686@gmail.com>
2022-07-04 14:56:46 -0400

    add event listener

--- website/app.js
+++ website/app.js
@@ -1,10 +1,17 @@
 /* Global Variables */
 
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip='
+const apiKey = '' 
+
 // Create a new date instance dynamically with JS
 let d = new Date();
-
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
-const postData = async (url = "", data = {}) => {
+
+// Eventlistener to add function for the dom element
+document.getElementById('generate').addEventListener('click',runEvent);
+
+// Function to POST data from the lessons
+async function postData(url = "", data = {}) {
 	console.log(data);
 	const response = await fetch(url, {
 		method: "POST",
@@ -23,6 +30,6 @@ const postData = async (url = "", data = {}) => {
 	} catch (error) {
 		console.log("error", error);
 	}
-};
+}
 
 postData("/add", { answer: 42 });
