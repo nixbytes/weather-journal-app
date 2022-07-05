a6256ce7bb0df13ef00a87ee002651d1b121b602
nixbytes <real8686@gmail.com>
2022-07-05 12:18:58 -0400

    add weather functions for api call and return json

--- website/app.js
+++ website/app.js
@@ -1,14 +1,37 @@
 /* Global Variables */
 
-const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip='
-const apiKey = '' 
+const urlLink = 'https://api.openweathermap.org/data/2.5/weather?zip='
+const apiKey = ''
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
 // Eventlistener to add function for the dom element
-document.getElementById('generate').addEventListener('click',runEvent);
+document.getElementById('generate').addEventListener('click', runEvent);
+
+function runEvent() {
+	// get data from user
+	const getZip = document.getElementById('zip').value;
+	const content = document.getElementById('feelings').value;
+	getWeather(urlLink, getZip, apiKey);
+
+}
+
+/* Function to call and get Web API Data*/
+const getWeather = async (urlLink, getZip, apiKey) => {
+	// response from api fetch data 
+	const response = await fetch(urlLink + getZip + apiKey);
+	try {
+		// try fetch json from api call
+		const jsonData = await response.json();
+		return jsonData;
+	} catch (error) {
+		// check for error
+		console.log("error", error);
+	}
+}
+
 
 // Function to POST data from the lessons
 async function postData(url = "", data = {}) {
